5759933a8156112fc43ed093b5d8db22bf55799f
MariamBAMBA <mariam.bamba@ynov.com>
2025-01-10 12:34:49 +0100

    footer dynamique

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Script chargé');
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Script chargé");
+  document.getElementById("infoButton").onclick = function () {
+    const infoContainer = document.getElementById("infoContainer");
+    if (infoContainer.style.display === "none") {
+      infoContainer.style.display = "block";
+      infoContainer.innerText = `Date et Heure : ${new Date().toLocaleString()}`;
+      this.textContent = "Masquer les infos";
+    } else {
+      infoContainer.style.display = "none";
+      this.textContent = "Afficher les infos";
+    }
+  };
 });
